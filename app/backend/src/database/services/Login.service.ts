e40923b274c132a4e175fda6fbc13d19b74e7c60
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-10-23 19:52:55 -0300

    feat: loginService, loginRoutes

---
+++ app/backend/src/database/services/Login.service.ts
@@ -0,0 +1,16 @@
+import User from '../models/User.model';
+import { IUser } from '../interfaces/IUser';
+import decodeJwt from '../utils/decodeToken';
+
+export default class LoginService {
+  getByEmail = (email: string): Promise<IUser | null> => {
+    const getUser = User.findOne({ where: { email } });
+    return getUser;
+  };
+
+  getRole = (token: string) => {
+    const { role } = decodeJwt(token);
+
+    return { role };
+  };
+}
