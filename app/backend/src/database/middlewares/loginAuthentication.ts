8aba02aa0db076caf9d211fb9d3d87bb708e5f80
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-10-24 15:00:58 -0300

    feat: authentication middleware

--- app/backend/src/database/middlewares/loginAuthentication.ts
+++ app/backend/src/database/middlewares/loginAuthentication.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { compare } from 'bcryptjs';
 import Service from '../services/Login.service';
+import CreateToken from '../utils/createToken';
 
 const serviceLogin = new Service();
 
@@ -11,6 +13,18 @@ const authentication = async (req: Request, res: Response, next: NextFunction) =
   const user = await serviceLogin.getByEmail(email);
 
   if (!user) return res.status(401).json({ message: 'Incorrect email or password' });
+
+  const comparePassword = await compare(password, user.password);
+
+  if (!comparePassword) return res.status(401).json({ message: 'Incorrect email or password' });
+
+  const userRole = {
+    role: user.role,
+    password: user.password,
+  };
+
+  const token = CreateToken(userRole);
+  res.status(200).json({ token });
   next();
 };
 
