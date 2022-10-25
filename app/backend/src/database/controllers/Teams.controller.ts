a9ae9b4f9ac2840162e31fa3f8a301daebcb5186
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-10-25 17:07:27 -0300

    fix: exclusão do serviceTeams

--- app/backend/src/database/controllers/Teams.controller.ts
+++ app/backend/src/database/controllers/Teams.controller.ts
@@ -1,23 +1,17 @@
 import { Request, Response } from 'express';
-import Service from '../services/Teams.service';
+import Teams from '../models/Teams.model';
 
 class TeamsController {
-  private teamsService: Service;
-
-  constructor() {
-    this.teamsService = new Service();
-  }
-
-  getTeams(_req: Request, res: Response) {
-    const teams = this.teamsService.getTeams();
+  getTeams = (req: Request, res: Response) => {
+    const teams = Teams.findAll();
     return res.status(200).json(teams);
-  }
+  };
 
-  getTeamsById(req: Request, res: Response) {
+  getTeamsById = (req: Request, res: Response) => {
     const { id } = req.params;
-    const teams = this.teamsService.getTeamsById(Number(id));
+    const teams = Teams.findByPk(id);
     return res.status(200).json(teams);
-  }
+  };
 }
 
 export default new TeamsController();
