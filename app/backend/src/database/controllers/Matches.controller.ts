217ca8339ec9aa78d76bc6e6a1c63c609ba572a1
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-10-26 15:23:24 -0300

    req: 23 validação do token

--- app/backend/src/database/controllers/Matches.controller.ts
+++ app/backend/src/database/controllers/Matches.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import MatchesService from '../services/Matches.service';
+import decodeJwt from '../utils/decodeToken';
 
 class MachesController {
   getMatches = async (req: Request, res: Response) => {
@@ -13,8 +14,19 @@ class MachesController {
   };
 
   createNewMatches = async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
     const matchBody = req.body;
+    try {
+      if (!authorization) {
+        return res.status(401).json({ message: 'Invalid authorization' });
+      }
+      decodeJwt(authorization);
+    } catch {
+      return res.status(401).json({ message: 'Invalid authorization' });
+    }
+
     const newMatch = await MatchesService.createNewMatch(matchBody);
+
     return res.status(201).json(newMatch);
   };
 }
--- app/backend/src/database/routes/MatchesRoutes.ts
+++ app/backend/src/database/routes/MatchesRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import Controller from '../controllers/Matches.controller';
+// import ValidateLogin from '../middlewares/loginAuthentication';
 
 const router = Router();
+// const validateLogin = new ValidateLogin();
 
 router.get('/', Controller.getMatches);
+router.post('/', Controller.createNewMatches);
 
 export default router;
